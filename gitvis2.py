c33f17a6ea2835db2fa9127e2f618577915f68d2
Nichodon <brandonsychung@gmail.com>
2018-02-27 12:47:59 -0800

    add lines to dots

--- gitvis2.py
+++ gitvis2.py
@@ -4,11 +4,19 @@ from Tkinter import *
 import math
 import time
 
-repo = git.Repo('.')
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+repo = git.Repo('../nichodon.github.io')
 
 commits = list(repo.iter_commits())
-children = {}
 lanes = {}
+positions = {}
+children = {}
 
 
 def leading(x):
@@ -42,7 +50,7 @@ iText.grid(row=0, column=0, sticky=EW)
 graph = LabelFrame(root, text='Graph')
 graph.grid(row=0, column=1)
 
-canvas = Canvas(graph, width=750, height=250, bg='white', scrollregion=(0, 0, 1000, 1000))
+canvas = Canvas(graph, width=750, height=250, bg='white', scrollregion=(0, 0, 1000, 10000))
 canvas.grid(row=0, column=0)
 
 status = LabelFrame(root, text='Status')
@@ -65,37 +73,40 @@ def replace(a, b):
     lanes[a] = min(lanes[a], b) if a in lanes else b
 
 
-y = 20
+e = 20
 used = []
 for i in range(len(commits)):
     commit = commits[i]
-    d = 0
-    if commit.hexsha in lanes:
-        d = lanes[commit.hexsha]
-    else:
-        d = follow()
-        lanes[commit.hexsha] = d
+    d = lanes[commit.hexsha] if commit.hexsha in lanes else follow()
+    lanes[commit.hexsha] = d
+    d *= 10
     first = 0
     for parent in commit.parents:
+        if parent.hexsha in children:
+            children[parent.hexsha].append(commit.hexsha)
+        else:
+            children[parent.hexsha] = [commit.hexsha]
         replace(parent.hexsha, lanes[commit.hexsha] if first == 0 else follow())
         first += 1
-    pprint.pprint(children)
-    d *= 20
-    tag = canvas.create_rectangle(10, y - 10, 740, y + 10,
-                                  fill='#eee' if (y / 20) % 2 == 1 else 'white', outline='', tags='t' + str(i))
-    canvas.create_oval(15 + d, y - 5, 25 + d, y + 5, fill='red', tags='t' + str(i))
-    canvas.create_text(200, y, text=commit.hexsha[:7], anchor=W, tags='t' + str(i))
-    canvas.create_text(300, y, text=commit.message.split('\n')[0], anchor=W, tags='t' + str(i))
-    canvas.create_text(550, y, text=commit.author.name, anchor=W, tags='t' + str(i))
-    canvas.create_text(650, y, text=year(commit.authored_date), anchor=W, tags='t' + str(i))
+    tag = canvas.create_rectangle(10, e - 10, 740, e + 10,
+                                  fill='#eee' if (e / 20) % 2 == 1 else 'white', outline='', tags='t' + str(i))
+    positions[commit.hexsha] = Point(d + 20, e)
+    if commit.hexsha in children:
+        for child in children[commit.hexsha]:
+            canvas.create_line(d + 20, e, positions[child].x, positions[child].y)
+    canvas.create_oval(15 + d, e - 5, 25 + d, e + 5, fill='red', tags='t' + str(i))
+    canvas.create_text(200, e, text=commit.hexsha[:7], anchor=W, tags='t' + str(i))
+    canvas.create_text(300, e, text=commit.message.split('\n')[0], anchor=W, tags='t' + str(i))
+    canvas.create_text(550, e, text=commit.author.name, anchor=W, tags='t' + str(i))
+    canvas.create_text(650, e, text=year(commit.authored_date), anchor=W, tags='t' + str(i))
     canvas.tag_bind('t' + str(i), '<Button-1>', click)
-    y += 20
+    e += 20
 
 
 def mousewheel(event):
-    canvas.yview_scroll(int(-math.copysign(1, event.delta)), "units")
+    canvas.yview_scroll(int(-math.copysign(1, event.delta)), 'units')
 
 
-root.bind_all("<MouseWheel>", mousewheel)
+root.bind_all('<MouseWheel>', mousewheel)
 
 mainloop()
