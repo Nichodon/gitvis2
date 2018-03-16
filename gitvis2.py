76454dca7ad20f50534eb114b762ec8a7a734792
Nichodon <brandonsychung@gmail.com>
2018-03-16 16:22:17 -0700

    fix graphing algorithm

--- gitvis2.py
+++ gitvis2.py
@@ -3,6 +3,7 @@ import pprint
 from Tkinter import *
 import math
 import time
+import random
 
 
 class Point:
@@ -65,39 +66,71 @@ sText.grid(row=0, column=0)
 
 
 def connect(p1, p2, q1, q2):
+    r = lambda: random.randint(0,255)
+    a1 = q1
+    b1 = p1
+    a2 = q2
+    b2 = p2
     if p1 < q1:
-        canvas.create_line(p1, p2, q1, p2)
-        canvas.create_line(q1, p2, q1, q2)
+        a1 = p1
+        b1 = q1
+        a2 = p2
+        b2 = q2
+    if a1 == b1:
+        canvas.create_line(a1, a2, b1, b2, fill='#%02X%02X%02X' % (r(),r(),r()))
+    elif a2 < b2:
+        canvas.create_line(a1, a2, b1 - 5, a2)
+        canvas.create_line(b1 - 5, a2, b1, a2 + 5)
+        canvas.create_line(b1, a2 + 5, b1, b2)
     else:
-        canvas.create_line(p1, p2, p1, q2)
-        canvas.create_line(p1, q2, q1, q2)
+        canvas.create_line(a1, a2, b1 - 5, a2)
+        canvas.create_line(b1 - 5, a2, b1, a2 - 5)
+        canvas.create_line(b1, a2 - 5, b1, b2)
 
 
 def follow():
     n = 0
     while True:
-        if n not in used or used[n] <= 0:
-            used[n] = 0
+        if n not in big or len(big[n]) == 0:
             return n
         n += 1
 
 
 e = 20
-used = {}
+used = []
 lane = 0
+big = {}
 for i in range(len(commits)):
     commit = commits[i]
-    print commit.hexsha
     lane = lanes[commit.hexsha] if commit.hexsha in lanes else 0
-    used[lane] = used[lane] - 1 if lane in used else 0
+    '''if commit.hexsha in children:
+        for child in children[commit.hexsha]:
+            print lanes[child] if child in lanes else "nah",
+            if child in lanes and lanes[child] != lane and lanes[child] in used:
+                used.remove(lanes[child])
+        print "      ::::::::" + str(lane) + "   " + str(used)'''
+    pprint.pprint(big)
+    if lane in big:
+        big[lane].remove(commit.hexsha)
+    x = 0
     for parent in commit.parents:
         overflow = follow()
-        lanes[parent.hexsha] = min(lanes[parent.hexsha], overflow) if parent.hexsha in lanes else overflow
-        used[overflow] += 1
+        if x == 0:
+            overflow = lane
+        if parent.hexsha in lanes:
+            big[lanes[parent.hexsha]].remove(parent.hexsha)
+        lanes[parent.hexsha] = overflow
+        if overflow in big:
+            big[overflow].append(parent.hexsha)
+        else:
+            big[overflow] = [parent.hexsha]
+        if overflow not in used:
+            used.append(overflow)
         if parent.hexsha in children:
             children[parent.hexsha].append(commit.hexsha)
         else:
             children[parent.hexsha] = [commit.hexsha]
+        x += 1
     lane *= 10
     tag = canvas.create_rectangle(10, e - 10, 790, e + 10,
                                   fill='#eee' if (e / 20) % 2 == 1 else 'white', outline='', tags='t' + str(i))
